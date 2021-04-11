ab16ef8f3d4a6b8feee10b6b1676da299e75af0f
cfornetti <77817531+cfornetti@users.noreply.github.com>
2021-04-11 15:52:33 -0700

    Added the GitHub Rep link

--- src/App.js
+++ src/App.js
@@ -4,7 +4,14 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      Weather App
+      <h1>Weather App</h1>
+      <footer>
+        This project is created by Chiara Fornetti and is {" "}
+      <a href="https://github.com/cfornetti/react-weather-app"
+      target="_blank"
+      > open-sourced on GitHub
+      </a>
+      </footer>
     </div>
   );
 }
